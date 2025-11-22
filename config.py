fa1ffd639f4cc9ca9f1b7fe2f07be64c8d58f106
ibrohim2505 <ibrohim2505@users.noreply.github.com>
2025-11-22 07:21:39 +0500

    Update bot token to new bot

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Bot konfiguratsiyasi
 
 # Bot Token
-BOT_TOKEN = "8191463893:AAG9RfKgyvPToEUbn8lzheKH9UWxarFIpXY"
+BOT_TOKEN = "8353739338:AAG9d_9DvVJerPt-gWgGSnoyL9WUZlU66LU"
 
 # Admin ID
 ADMIN_ID = 5425876649
